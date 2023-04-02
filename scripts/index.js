8a3540be43a469795195c0c61f7079eaf3352eea
Santjago Ulndreaj <santiagoulndreaj1@gmail.com>
2023-04-02 18:39:00 +0200

    JS_fixed loading screen

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,25 @@
-let obj2 = document.getElementById("loadingNum");
+let obj2;
 function animateValue(obj, start, end, duration) {
     let startTimestamp = null;
     const step = (timestamp) => {
       if (!startTimestamp) startTimestamp = timestamp;
       const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-      obj.innerHTML = Math.floor(progress * (end - start) + start) + "%";
+      obj.innerText = Math.floor(progress * (end - start) + start) + "%";
       if (progress < 1) {
         window.requestAnimationFrame(step);
       }
     };
     window.requestAnimationFrame(step);
   }
-window.onload = function() {
-    animateValue(obj2, 0, 100, 5000);
-};
+
+  window.onload = (event) => {
+    obj2 = document.getElementById("loadingNum");
+    animateValue(obj2, 0, 100, 4000);
+    setTimeout(() => {
+        document.querySelector('.loading').classList.toggle('false');
+        setTimeout(() => {
+            document.getElementById('loading').style.display = 'none';
+        }, 1000);
+    }, 4000);
+  };
+  
